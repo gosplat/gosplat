4281d0539ae22dd245339f1c4372112c843dc6cf
Noah Hakansson <noah.hakansson@protonmail.com>
2023-02-12 18:23:37 +0100

    Clean up python files and seperate functionality to functions.

--- python_helper/compareHelper.py
+++ python_helper/compareHelper.py
@@ -1,25 +1,40 @@
 # Provides helper functions for doing comparisons using the provided model.
 from gensim.models import FastText
-from colors import green, red, blue
+from colors import green, red, blue, yellow
 
 def compare_package_function_list_distance(package_name: str, function_list: list[str], model: FastText):
     """
     Checks a list of functions against a package name
     and compares the distance between each function and the package name.
+    Prints the results.
     """
     if not function_list:
         # if list is empty
         print(red(f"Error: No functions in package {package_name}."))
         return
 
+    print(blue(f"Results for package '{package_name}':"))
+    for function in function_list:
+        print(yellow(f"\tFunction '{function}':"), green(model.wv.distance(package_name, function)))
+
+
+def list_most_similar(function_list: list[str], model: FastText):
+    """
+    Takes function_list and for each function
+    checks for most similar words in training data,
+    prints list of most similar word.
+    """
     for function in function_list:
         print(blue(f"Results for words found similar to \"{function}\""))
         print(green(model.wv.most_similar(function)))
 
-    for function in function_list:
-        print(blue(f"Results for package name '{package_name}', compared to '{function}'"))
-        print(green(model.wv.distance(package_name, function)))
-    
+
+def find_non_matching_function(function_list: list[str], model: FastText):
+    """
+    Takes function_list
+    Finds function in list that matches the least with the other functions.
+    Prints the name of that function.
+    """
     print(blue("Results for word that least fits in given word list"))
     print(red(model.wv.doesnt_match(function_list)))
 
--- python_helper/fastModelCompare.py
+++ python_helper/fastModelCompare.py
@@ -2,9 +2,9 @@
 import argparse
 import json
 from typing import Any
-from colors import yellow
+from colors import green, red, blue, yellow
 from gensim.models.fasttext import load_facebook_model
-from compareHelper import *
+from compareHelper import compare_package_function_list_distance
 
 def main():
     # command line arguments
@@ -21,34 +21,34 @@ def main():
     model = load_facebook_model(args.model_file)
     
     if args.project_json != "none":
-        # TODO: parse object and compare each packages functions against the package name
+        ## parse data into dictionary
         project_data:dict[str,Any] = json.loads(project_json)
         print(blue(project_data))
         # package_functions: Dictionary with key package name, containing all package functions.
         package_functions:dict[str,list[str]] = {}
         # get a packages functions and add them to dictionary.
         for package in project_data:
-            print(blue(package))
-            print(green(project_data[package]))
+            # print(blue(package))
+            # print(green(project_data[package]))
             package_files = project_data[package]["package_files"]
-            print(red(package_files))
+            # print(red(package_files))
             function_list:list[str] = []
             for file in package_files:
-                functions = file["functions"]
-                function_list.extend(functions)
-            print(yellow(function_list))
+                functions:list[str] = file["functions"]
+                if functions:
+                    function_list.extend(functions)
+            # print(yellow(function_list))
             # add function_list to current package in dictionary.
             package_functions[package.lower()] = function_list
 
-            print(red(project_data[package]["package_files"]))
+            # print(red(project_data[package]["package_files"]))
         
+        ## Present Results
         # compare function names to the package name they exists in.
         for package_name in package_functions:
-            print(green(package_name))
             compare_package_function_list_distance(package_name, package_functions[package_name], model)
-        print(blue(package_functions))
         # test
-        compare_package_function_list_distance("database", [], model)
+        compare_package_function_list_distance("routes", ["getuser", "execpythonmodel"], model)
 
         return
 
