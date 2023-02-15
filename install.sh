391801d3b81357267dc5f22488293e56280fb3c9
Gabriel Ivarsson <77668883+Gabriel-Ivarsson@users.noreply.github.com>
2023-02-15 15:56:48 +0100

    Update install.sh
    Co-authored-by: Flex <noah.hakansson@protonmail.com>

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@
 #
 # Updating and moving dependencies to $HOME/.local/share/
 git submodule update --init --recursive
-cp -r ../gopslat $HOME/.local/share
+mkdir -p $HOME/.local/share/gosplat
+cp -r ./python_helper $HOME/.local/share/gosplat
 
 # Building program binary
 go install .
