561166045f752ba65910c42b6c45c6eafb306def
Flex <noah.hakansson@protonmail.com>
2023-02-15 13:44:13 +0100

    No function sanitizing in GO JSON output (#2)
    * Remove name sanitizing from GO code.
    
    * Add `.lower()` to all strings in model comparisons
    
    * Add regexp function to sanitize names and lowercase them before feeding them to model.

--- python_helper/fastModelCompare.py
+++ python_helper/fastModelCompare.py
@@ -2,9 +2,9 @@
 import argparse
 import json
 
-from colors import blue, green, red, yellow
-from compareHelper import (compare_package_function_list_distance,
-                           find_best_matching_package, list_best_matching_package)
+from colors import blue
+from compareHelper import (
+    compare_package_function_list_distance, list_best_matching_package)
 from gensim.models.fasttext import load_facebook_model
 
 
@@ -68,7 +68,7 @@ def main():
 
         # test
         compare_package_function_list_distance(
-            "routes", ["getuser", "execpythonmodel"], model)
+            "pythonrunner", ["getuser", "ExecPythonModel"], model)
 
         return
 
