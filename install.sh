e6939ef5fd24638e7b9f820372fe5ed2fa52b20f
Gabriel-Ivarsson <DingusDungus@github.com>
2023-03-01 19:26:44 +0100

    Finalized install script, done this time i swear  - better model storage  - Fully working installation

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 # Install script for gosplat
 #
 # Updating and moving dependencies to $HOME/.local/share/
-./src/modelGetter/modelGetter
+mkdir -p $HOME/.local/share/gosplat/src
+git clone https://gitlab.com/gaiv20/gosplat-models.git
+mv ./gosplat-models/fast-fb-model.bin $HOME/.local/share/gosplat
 
 mkdir -p $HOME/.local/share/gosplat
-rsync -av --progress ./* $HOME/.local/share/gosplat --exclude ./.git
+rsync -av --progress ./src/python_helper $HOME/.local/share/gosplat/src --exclude ./.git
 # Building program binary
-go install .
+go build -o gosplat .
+mv gosplat $HOME/.local/bin/
