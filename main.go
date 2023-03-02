52c24c8250e045f481c0b7e0de6d75947837f930
Gabriel-Ivarsson <DingusDungus@github.com>
2023-03-02 21:42:57 +0100

    Fixed program output and simple checks
     - Program now checks for if input actually exists, if not do early
       return and print help text
     - Simple input parsing, could be refactored in the future if we feel
       that we require more support for flags but for now is all right
     - Cleaned up left-over dev artifacts
     - Cleaned up and improved output of model

--- main.go
+++ main.go
@@ -9,15 +9,42 @@ import (
 	"github.com/NoahHakansson/gosplat/src/toolParser"
 )
 
+func printHelp() {
+	fmt.Printf("Gosplat v1\n" +
+		" gosplat --h , prints commands\n" +
+		" gosplat [input dir/file] , runs directory against model\n")
+}
+
+func checkForFlags(args []string) uint8 {
+	for _, arg := range args {
+		if arg == "--h" {
+			printHelp()
+			return earlyReturn
+		}
+	}
+	return continueProgram
+}
+
 var (
 	pythonPath = os.Getenv("HOME") + "/.local/share/gosplat/src/python_helper/fast_model_compare.py"
 	modelBin   = os.Getenv("HOME") + "/.local/share/gosplat/fast-fb-model.bin"
-	// pythonPath = "src/python_helper/fast_model_compare.py"
-	// modelBin   = "fast-fb-model.bin"
+)
+
+const (
+	earlyReturn     uint8 = 1
+	continueProgram uint8 = 2
 )
 
 func main() {
 	dir := os.Args[1]
+	if checkForFlags(os.Args) == earlyReturn {
+		return
+	}
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		fmt.Println("Error; Input was not a directory or file")
+		printHelp()
+		return
+	}
 	toolParser.ParseDir(dir)
 	jsonData, err := toolParser.GenerateInputJSON()
 	if err != nil {
